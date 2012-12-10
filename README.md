9d30ad09f6d692b2d3bbcb5717ca2a50bb2e9a90
Rafael Kitover <rkitover@cpan.org>
2012-12-10 07:29:29 -0500

    version 0.0.1

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 git-svn-hooks
 =============
 
-run hooks for git-svn commands
+This is a shell function for Bourne compatible shells (bash, zsh, ksh, ash,
+etc.) that allows you to run hooks for git-svn commands (including through
+aliases.)
+
+* Installation
+
+    mkdir ~/.sh
+    mv git-svn.sh ~/.sh
+
+In your shell startup file (e.g. ~/.bashrc) add the following:
+
+    for f in ~/.sh/*.sh; do
+        source $f
+    done
+
+* Hooks
+
+You can make pre and post hooks, they go into .git/hooks/ in your project
+directory. For example:
+
+    .git/hooks/post-svn-dcommit
+    .git/hooks/pre-svn-rebase
+
+The hooks should be executable files (such as chmod +x shell scripts) that
+return 0 on success and some other value on failure.
+
+A pre hook that returns failure will stop the git-svn command from executing.
+
+* Usage
+
+Use git and git-svn commands as usual, including aliases you have for them,
+the git-svn hooks will run.
---
+++ git-svn.sh
@@ -0,0 +1,83 @@
+# git-svn-hooks -- a helper for git-svn that allows hooks
+#
+# Version: 0.0.1
+#
+# Works with all Bourne compatible shells.
+#
+# Based on: https://raw.github.com/hkjels/.dotfiles/master/zsh/git-svn.zsh
+#
+# Author: rkitover: Rafael Kitover (rkitover@cpan.org)
+
+git() {
+    _root=$(command git rev-parse --show-toplevel);
+
+    # check that this is a git-svn repo
+    [ -d "$_root/.git/svn"  ] && [ x != x"$(ls -A "$_root/.git/svn/")" ] && _git_svn=1
+
+    # otherwise just pass through to git
+    if [ -z "$_git_svn" ]; then
+        command git "$@"
+        unset _root _git_svn
+        return $?
+    fi
+
+    unset _git_svn
+
+    # Expand git aliases
+    _param_1=$1
+    export _param_1
+
+    _expanded="$( \
+        command git config --get-regexp alias | sed -e 's/^alias\.//' | while read _alias _git_cmd; do \
+            if [ "$_alias" = "$_param_1" ]; then \
+                echo "$_git_cmd"; \
+                break; \
+            fi; \
+        done \
+    )"
+
+    unset _param_1
+
+    if [ -n "$_expanded" ]; then
+        shift;
+        eval "git $_expanded \"\$@\""
+        unset _expanded _root
+        return $?
+    fi
+
+    unset _expanded
+
+    if [ "$1" != "svn" ]; then
+        command git "$@"
+        unset _root
+        return $?
+    else
+        shift;
+    fi
+
+    # Pre hooks
+    if [ -x "$_root/.git/hooks/pre-svn-$1" ]; then
+        if ! "$_root"/.git/hooks/pre-svn-$1; then
+            unset _root
+            return $?
+        fi
+    fi
+
+    # call git-svn
+    command git svn "$@"
+    _exit_val=$?
+
+    # Post hooks
+    if [ -x "$_root/.git/hooks/post-svn-$1" ]; then
+        if ! "$_root"/.git/hooks/post-svn-$1; then
+            unset _root _exit_val
+            return $?
+        fi
+    fi
+
+    unset _root
+
+    sh -c "exit $_exit_val"
+    unset _exit_val
+    return $?
+}
