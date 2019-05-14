56223dbebc5f5ade33149b358d7c67a84ac2b0ab
Geraint White <geraintwhite@gmail.com>
2019-05-14 11:04:17 +0100

    Improve alias expansion

--- git-svn.sh
+++ git-svn.sh
@@ -31,14 +31,7 @@ git() {
     _param_1=$1
     export _param_1
 
-    _expanded="$( \
-        command git config --get-regexp alias | sed -e 's/^alias\.//' | while read -r _alias _git_cmd; do \
-            if [ "$_alias" = "$_param_1" ]; then \
-                echo "$_git_cmd"; \
-                break; \
-            fi; \
-        done \
-    )"
+    _expanded="$(command git config --get-regexp ^alias.$_param_1$ | sed -e 's/[^ ]* //')"
 
     unset _param_1
 
@@ -141,10 +134,10 @@ git() {
 
 # Copyright (c) 2013, Rafael Kitover
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # Redistributions of source code must retain the above copyright notice, this
 # list of conditions and the following disclaimer.
 #
